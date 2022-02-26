1f59ecb71305bb8d89c8a6c457d442f7808047f4
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-02-26 15:16:47 -0500

    context provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import './App.css';
-import Nav from './components/Nav';
-import Nav2 from './components/Nav2';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import { Routes, Route } from 'react-router-dom';
-
-import React from 'react';
+import "./App.css";
+import Nav from "./components/Nav";
+import Nav2 from "./components/Nav2";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import { Routes, Route } from "react-router-dom";
+import UserContext from "./context/UserContext";
+import { useState } from "react";
+import React from "react";
 
 const App = () => {
-	return (
-		<div>
-			<Nav2 />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-			</Routes>
-			<Nav />
-		</div>
-	);
+  const [user, setUser] = useState("");
+  return (
+    <div>
+      <UserContext.Provider value={user}>
+        <Nav2 />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Nav />
+      </UserContext.Provider>
+    </div>
+  );
 };
 
 export default App;
